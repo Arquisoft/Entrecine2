bd01d3775557da0f916b557e24cb082d7aa3478a
labra <labra@uniovi.es>
2013-04-11 10:04:23 +0200

    Added Sample User Stories with Selenium

---
+++ src/test/java/es/uniovi/asw/entrecine/PruebaGoogle.java
@@ -0,0 +1,68 @@
+package es.uniovi.asw.entrecine;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.htmlunit.*;
+import org.openqa.selenium.support.ui.Select;
+
+
+public class PruebaGoogle {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new HtmlUnitDriver();
+    baseUrl = "http://www.google.es/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testPruebaGoogle() throws Exception {
+    driver.get(baseUrl + "/");
+    WebElement element = driver.findElement(By.name("q"));
+    element.sendKeys("Hola");
+	element.submit();
+    assertEquals("Hola - Buscar con Google", driver.getTitle());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alert.getText();
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}

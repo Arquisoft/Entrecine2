46abe2d2a9cecf98cf904ac187958a1fb7fe165f
belaustegui <cristian.alvb@gmail.com>
2013-04-22 16:58:34 +0200

    Creado el acceso para el administrador.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,5 +10,9 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
+
+    public static Result adminIndex() {
+    	return ok(admin.render());
+    }
   
 }

55e2fef4c984316155ec03572a83244fddba94f2
belaustegui <cristian.alvb@gmail.com>
2013-04-23 21:31:35 +0200

    Anadido formulario de login en la pagina de inicio. Anadido metodo para generar usuarios en la base de datos (consultar routes).

--- app/controllers/Usuarios.java
+++ app/controllers/Usuarios.java
@@ -2,18 +2,50 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.data.Form;
+import models.Cliente;
+import models.Empleado;
+import models.Pelicula;
 import views.html.index;
 
 public class Usuarios extends Controller {
+
+	private static Form<Cliente> formCliente = Form.form(Cliente.class);
   
     public static Result index() {
-        // return ok(index.render());
-    	return(TODO);
+        return ok(index.render(Pelicula.findAll(), formCliente));
     }
 
     public static Result login() {
-        // return ok(index.render());
-    	return(TODO);
+    	Form<Cliente> formularioRecibido = formCliente.bindFromRequest();
+    	
+    	String login = formularioRecibido.field("login").value();
+    	String password = formularioRecibido.field("password").value();
+    	
+    	Cliente cliente = Cliente.findByLogin(login);
+    	if (cliente == null || !password.equals(cliente.getPassword())) {
+    		return badRequest(index.render(Pelicula.findAll(), formCliente));
+    	} else {
+    		return redirect(routes.Usuarios.index());
+    	}
+//        return ok(index.render());
+//    	return(TODO);
+    }
+    
+    public static Result rellenarDb() {
+    	Cliente cliente = new Cliente();
+    	cliente.setPassword("pass");
+    	cliente.setLogin("login");
+    	cliente.save();
+    	Empleado empleado = new Empleado();
+    	empleado.setPassword("pass");
+    	empleado.setLogin("empleado");
+    	empleado.save();
+    	Empleado admin = new Empleado();
+    	admin.setPassword("pass");
+    	admin.setLogin("admin");
+    	admin.save();
+    	return redirect(routes.Usuarios.index());
     }
   
 }

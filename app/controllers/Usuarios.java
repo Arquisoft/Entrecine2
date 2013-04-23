0edcfecc604377cf2b59ff25c4e46fe82e00e344
belaustegui <cristian.alvb@gmail.com>
2013-04-24 00:10:20 +0200

    Merge

--- app/controllers/Usuarios.java
+++ app/controllers/Usuarios.java
@@ -12,39 +12,43 @@ public class Usuarios extends Controller {
 
 	private static Form<Cliente> formCliente = Form.form(Cliente.class);
 
-	public static Result index() {
-		return ok(index.render(Pelicula.findAll(), formCliente));
-	}
+  
+    public static Result index() {
+        return ok(index.render(Pelicula.findAll(), formCliente));
+    }
 
-	public static Result login() {
-		Form<Cliente> formularioRecibido = formCliente.bindFromRequest();
-
-		String login = formularioRecibido.field("login").value();
-		String password = formularioRecibido.field("password").value();
-
-		Cliente cliente = Cliente.findByLogin(login);
-
-		if (cliente == null || !password.equals(cliente.getPassword())) {
-			return badRequest(index.render(Pelicula.findAll(), formCliente));
-		} else {
-			return redirect(routes.Usuarios.index());
-		}
-	}
-
-	public static Result rellenarDb() {
-		Cliente cliente = new Cliente();
-		cliente.setPassword("pass");
-		cliente.setLogin("login");
-		cliente.save();
-		Empleado empleado = new Empleado();
-		empleado.setPassword("pass");
-		empleado.setLogin("empleado");
-		empleado.save();
-		Empleado admin = new Empleado();
-		admin.setPassword("pass");
-		admin.setLogin("admin");
-		admin.save();
-		return redirect(routes.Usuarios.index());
-	}
-
+    public static Result login() {
+    	Form<Cliente> formularioRecibido = formCliente.bindFromRequest();
+    	
+    	String login = formularioRecibido.field("login").value();
+    	String password = formularioRecibido.field("password").value();
+    	
+        Cliente cliente = null;
+        for (Cliente c: Cliente.findAll()) {
+            if (c.getLogin().equals(login))
+                cliente = c;
+        }
+    	if (cliente == null || !password.equals(cliente.getPassword())) {
+    		return badRequest(index.render(Pelicula.findAll(), formCliente));
+    	} else {
+    		return redirect(routes.Usuarios.index());
+    	}
+    }
+    
+    public static Result rellenarDb() {
+    	Cliente cliente = new Cliente();
+    	cliente.setPassword("pass");
+    	cliente.setLogin("login");
+    	cliente.save();
+    	Empleado empleado = new Empleado();
+    	empleado.setPassword("pass");
+    	empleado.setLogin("empleado");
+    	empleado.save();
+    	Empleado admin = new Empleado();
+    	admin.setPassword("pass");
+    	admin.setLogin("admin");
+    	admin.save();
+    	return redirect(routes.Usuarios.index());
+    }
+  
 }

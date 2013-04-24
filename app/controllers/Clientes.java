8ce93cf572bd94347156769b35c1e1a703f69b00
BrunoLopezGarcia <uo219018@uniovi.es>
2013-04-24 19:17:25 +0200

    Cambiada la manera de acceder a la session en la vista index

--- app/controllers/Clientes.java
+++ app/controllers/Clientes.java
@@ -22,7 +22,7 @@ public class Clientes extends Controller {
 
 	public static Result index() {
 		Cliente cliente = Cliente.findByLogin(session().get("cliente"));
-		return ok(index.render(Pelicula.findAll(), formCliente, cliente));
+		return ok(index.render(Pelicula.findAll(), formCliente));
 	}
 
 	public static Result login() {
@@ -36,7 +36,7 @@ public class Clientes extends Controller {
 		if (cliente == null || !password.equals(cliente.getPassword())) {
 			//Solamente mostramos el error en login, asi no se sabe si el error lo dio porque no existe el usuario o porque la contraseña no coincide
 			formularioRecibido.reject("login", "El usuario o contraseña no es correcto");
-			return badRequest(index.render(Pelicula.findAll(), formularioRecibido, cliente));
+			return badRequest(index.render(Pelicula.findAll(), formularioRecibido));
 		} else {
 			session().put("cliente", login);
 			return redirect(routes.Clientes.index());
@@ -55,7 +55,7 @@ public class Clientes extends Controller {
 		
 
 		if (pelicula == null) {
-			return badRequest(index.render(Pelicula.findAll(), formCliente, cliente));
+			return badRequest(index.render(Pelicula.findAll(), formCliente));
 		} else {
 			return ok(vistaPelicula.render(pelicula, cliente));
 		}
